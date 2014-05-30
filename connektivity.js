7c0afcb47d17043ede021bbde28c98c06da6e458
zafar-saleem <zafarsaleem3@gmail.com>
2014-05-30 20:32:44 +0200

    change file

--- connektivity.js
+++ connektivity.js
@@ -1,29 +1,4 @@
-(function (win) {
-
+(function () {
 	'use strict';
-
-    if (!navigator.onLine) {
-        win.addEventListener('online',  updateOnlineStatus);
-        win.addEventListener('offline', updateOnlineStatus);
-    } else {
-        
-    }
-
-    var createEl = function (status) {
-        var el = document.createElement('<span />'),
-            body = document.getElementsByTagName('body');
-
-        el.className = status;
-        el.innerHTML = status.toUpperCase();
-
-        body.appendChild(el);
-    },
-
-    updateOnlineStatus = function (e) {
-        var status = document.getElementById('status'),
-            condition = navigator.onLine ? 'online' : 'offline';
-
-        createEl(condition);
-    };
-
-})(window);
+	
+})();
